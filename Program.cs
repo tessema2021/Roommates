b48440aea8ee825f8e2cb76b1279cd82499b8359
tessema2021 <ninafuchere@gmail.com>
2021-12-06 15:45:00 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using Roommates.Repositories;
+using Roommates.Models;
+namespace Roommates
+{
+    class Program
+    {
+        //  This is the address of the database.
+        //  We define it here as a constant since it will never change.
+        private const string CONNECTION_STRING = @"server=localhost\SQLExpress;database=Roommates;integrated security=true;TrustServerCertificate=true;";
+
+
+
+        static void Main(string[] args)
+        {
+
+            RoomRepository roomRepo = new RoomRepository(CONNECTION_STRING);
+
+            bool runProgram = true;
+            while (runProgram)
+            {
+                string selection = GetMenuSelection();
+
+                switch (selection)
+                {
+                    case ("Add a room"):
+                        Console.Write("Room name: ");
+                        string name = Console.ReadLine();
+
+                        Console.Write("Max occupancy: ");
+                        int max = int.Parse(Console.ReadLine());
+
+                        Room roomToAdd = new Room()
+                        {
+                            Name = name,
+                            MaxOccupancy = max
+                        };
+
+                        roomRepo.Insert(roomToAdd);
+
+                        Console.WriteLine($"{roomToAdd.Name} has been added and assigned an Id of {roomToAdd.Id}");
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+
+
+                }
+            }
+
+
+        }
+
+        static string GetMenuSelection()
+        {
+            Console.Clear();
+
+            List<string> options = new List<string>()
+            {
+                "Show all rooms",
+                "Search for room",
+                "Add a room",
+                "Exit"
+            };
+
+            for (int i = 0; i < options.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {options[i]}");
+            }
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine();
+                    Console.Write("Select an option > ");
+
+                    string input = Console.ReadLine();
+                    int index = int.Parse(input) - 1;
+                    return options[index];
+                }
+                catch (Exception)
+                {
+
+                    continue;
+                }
+            }
+        }
+    }
+}
+
